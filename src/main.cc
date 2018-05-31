4772cd0827e0ed4a70d4cd9e5433f6c25f4aa972
Dario Cazzani <dcazzani@cisco.com>
2018-05-31 14:00:16 -0700

    Main source file

---
+++ src/main.cc
@@ -0,0 +1,34 @@
+#include "nodes/Node.h"
+#include "nodes/Add.h"
+#include "nodes/Multiply.h"
+#include "nodes/Input.h"
+
+#include "graph/graph_utils.h"
+
+
+int main()
+{
+  Input i1;
+  Input i2;
+  Add a;
+  a.addInput(&i1);
+  a.addInput(&i2);
+  Multiply m;
+  m.addInput(&i1);
+  m.addInput(&i2);
+  vector<Node *> graph = {&i1, &i2, &a, &m};
+
+  buildGraph(graph);
+
+  map<Node*, double> inputMap;
+  inputMap[&i1] = 3.0;
+  inputMap[&i2] = 3.0;
+
+  vector<double> results = runGraph(graph, inputMap);
+
+  cout<<"Output of computation graph: \n";
+  for(auto r : results){
+    cout<<r<<" - ";
+  }
+  cout<<endl;
+}
