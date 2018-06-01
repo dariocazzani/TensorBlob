456a0e9008ade7cfb0045764e5f41407c35b2ceb
Dario Cazzani <dcazzani@cisco.com>
2018-06-01 11:33:38 -0700

    simple experiments for dynamic broadcasting

--- eigen.cc
+++ eigen.cc
@@ -2,44 +2,26 @@
 
 #include <iostream>
 
-void setValues(Eigen::VectorXd &values);
-void copy(const Eigen::VectorXd &values);
-
 using namespace std;
 int main()
 {
-  Eigen::MatrixXd weights;
-  // 2 features, 3 hidden neurons
-  weights.resize(3, 2);
-  weights << 1, 4,
-             2, 5,
-             3, 6;
+  Eigen::MatrixXd mat(2,4);
+  Eigen::MatrixXd v(1,4);
+  mat << 1, 2, 6, 9,
+         3, 1, 7, 2;
+  v << 0,
+       1,
+       2,
+       3;
+  assert(v.rows() == 1);
 
-  Eigen::MatrixXd inputs;
-  // 1 sample, 2 features
-  inputs.resize(1, 2);
-  inputs << 1, 2;
 
-  Eigen::MatrixXd bias;
-  bias.resize(3, 1);
-  bias << 1, 2, 3;
+  Eigen::Map<Eigen::VectorXd> v_vec(v.data(),v.size());
 
-  /*
-   * Equivalent to:
-   * np.dot(weights, inputs) + bias
-   */
-   
-  cout<<"Result: \n"<<weights * inputs.transpose() + bias<<endl;
-  return 0;
-}
 
-void setValues(Eigen::VectorXd &values) {
-  values.resize(2);
-  values << 3.0, 4.0;
-}
+  //add v to each column of m
+  mat.transpose().colwise() += v_vec;
 
-void copy(const Eigen::VectorXd &values) {
-  Eigen::VectorXd values2;
-  values2 = values;
-  std::cout<<values2<<std::endl;
+  std::cout << "Broadcasting result: " << std::endl;
+  std::cout << mat << std::endl;
 }
