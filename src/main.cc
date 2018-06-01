afa3d77ea04a33d626c10ee3adb88784a070b565
Dario Cazzani <dcazzani@cisco.com>
2018-06-01 11:33:07 -0700

    Cleaner code for main.cc

--- src/main.cc
+++ src/main.cc
@@ -5,38 +5,41 @@
 
 int main()
 {
-  Eigen::MatrixXd weights;
+  //INPUTS
+
   // 2 features, 3 hidden neurons
-  weights.resize(2, 3);
+  Eigen::MatrixXd weights(2, 3);
+  // weights.resize(2, 3);
   weights << 1, 2, 3,
              4, 5, 6;
-  Input W;
 
-  Eigen::MatrixXd inputs;
   // 4 sample, 2 features
-  inputs.resize(4, 2);
+  Eigen::MatrixXd inputs(4, 2);
   inputs << 1, 2,
             3, 4,
             5, 6,
             7, 8;
 
-  Input X;
-
-  Eigen::MatrixXd bias;
-  bias.resize(1, 3);
+  Eigen::MatrixXd bias(1,3);
   bias << 1, 2, 3;
 
+
+  // DEFINE NODES AND CONNECT THEM
+  Input W;
+  Input X;
   Input b;
 
+  // BUILD GRAPH
   Linear hidden1(&X, &W, &b);
-
   vector<Node *> graph = {&hidden1, &W, &b, &X};
   buildGraph(graph);
 
+  // FEED_DICT
   map<Node*, Eigen::MatrixXd> inputMap;
   inputMap[&W] = weights;
   inputMap[&b] = bias;
   inputMap[&X] = inputs;
+
 
   vector<Eigen::MatrixXd> results = forwardProp(&graph, inputMap);
   for(auto r : results){
