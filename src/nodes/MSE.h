a1f4142baf5eb261169e841f9bb09c41d259971c
Dario Cazzani <dcazzani@cisco.com>
2018-06-01 15:29:25 -0700

    Implemented MSE

---
+++ src/nodes/MSE.h
@@ -0,0 +1,38 @@
+#ifndef MSE_H
+#define MSE_H
+
+#include "Node.h"
+
+// NB Has exactly 2 inputs Nodes
+class MSE : public Node
+{
+public:
+  MSE(Node *activations, Node *reference);
+  void forward();
+};
+
+MSE::MSE(Node *activations, Node *reference)
+{
+  this->addInput(activations);
+  this->addInput(reference);
+}
+void MSE::forward()
+{
+  vector<Eigen::MatrixXd> inputs;
+  inputs = getInputValues();
+
+  Eigen::MatrixXd activations = inputs[0];
+  Eigen::MatrixXd reference = inputs[1];
+
+  if(!(activations.rows()==reference.rows() &&
+       activations.cols()==reference.cols())) {
+    throw invalid_argument("activation tensor and reference tensor must have the same shape");
+  }
+
+  double mse;
+  mse = (activations - reference).norm() / activations.size();
+
+  setValues(mse);
+}
+
+#endif
