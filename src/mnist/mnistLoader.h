c7879f7482fe6ed0aae8c0b9a3151d624ae07ff2
Dario Cazzani <dcazzani@cisco.com>
2018-06-05 23:11:30 -0700

    Stupid tabs

--- src/mnist/mnistLoader.h
+++ src/mnist/mnistLoader.h
@@ -32,8 +32,8 @@ extern "C" {
 #endif
 
 typedef struct mnist_data {
-	MNIST_DATA_TYPE data[28][28]; /* 28x28 data for the image */
-	unsigned int label; /* label : 0 to 9 */
+  MNIST_DATA_TYPE data[28][28]; /* 28x28 data for the image */
+  unsigned int label; /* label : 0 to 9 */
 } mnist_data;
 
 /*
@@ -42,10 +42,10 @@ typedef struct mnist_data {
 #ifdef MNIST_HDR_ONLY
 
 _STATIC int mnist_load(
-	const char *image_filename,
-	const char *label_filename,
-	mnist_data **data,
-	unsigned int *count);
+  const char *image_filename,
+  const char *label_filename,
+  mnist_data **data,
+  unsigned int *count);
 
 #else
 
@@ -59,15 +59,15 @@ _STATIC int mnist_load(
  */
 static unsigned int mnist_bin_to_int(char *v)
 {
-	int i;
-	unsigned int ret = 0;
+  int i;
+  unsigned int ret = 0;
 
-	for (i = 0; i < 4; ++i) {
-		ret <<= 8;
-		ret |= (unsigned char)v[i];
-	}
+  for (i = 0; i < 4; ++i) {
+  ret <<= 8;
+  ret |= (unsigned char)v[i];
+  }
 
-	return ret;
+  return ret;
 }
 
 /*
@@ -77,86 +77,86 @@ static unsigned int mnist_bin_to_int(char *v)
  * Check comments for the return codes.
  */
 _STATIC int mnist_load(
-	const char *image_filename,
-	const char *label_filename,
-	mnist_data **data,
-	unsigned int *count)
+  const char *image_filename,
+  const char *label_filename,
+  mnist_data **data,
+  unsigned int *count)
 {
-	int return_code = 0;
-	int i;
-	char tmp[4];
+  int return_code = 0;
+  int i;
+  char tmp[4];
 
-	unsigned int image_cnt, label_cnt;
-	unsigned int image_dim[2];
+  unsigned int image_cnt, label_cnt;
+  unsigned int image_dim[2];
 
-	FILE *ifp = fopen(image_filename, "rb");
-	FILE *lfp = fopen(label_filename, "rb");
+  FILE *ifp = fopen(image_filename, "rb");
+  FILE *lfp = fopen(label_filename, "rb");
 
-	if (!ifp || !lfp) {
-		return_code = -1; /* No such files */
-		goto cleanup;
-	}
+  if (!ifp || !lfp) {
+  return_code = -1; /* No such files */
+  goto cleanup;
+  }
 
-	fread(tmp, 1, 4, ifp);
-	if (mnist_bin_to_int(tmp) != 2051) {
-		return_code = -2; /* Not a valid image file */
-		goto cleanup;
-	}
+  fread(tmp, 1, 4, ifp);
+  if (mnist_bin_to_int(tmp) != 2051) {
+  return_code = -2; /* Not a valid image file */
+  goto cleanup;
+  }
 
-	fread(tmp, 1, 4, lfp);
-	if (mnist_bin_to_int(tmp) != 2049) {
-		return_code = -3; /* Not a valid label file */
-		goto cleanup;
-	}
+  fread(tmp, 1, 4, lfp);
+  if (mnist_bin_to_int(tmp) != 2049) {
+  return_code = -3; /* Not a valid label file */
+  goto cleanup;
+  }
 
-	fread(tmp, 1, 4, ifp);
-	image_cnt = mnist_bin_to_int(tmp);
+  fread(tmp, 1, 4, ifp);
+  image_cnt = mnist_bin_to_int(tmp);
 
-	fread(tmp, 1, 4, lfp);
-	label_cnt = mnist_bin_to_int(tmp);
+  fread(tmp, 1, 4, lfp);
+  label_cnt = mnist_bin_to_int(tmp);
 
-	if (image_cnt != label_cnt) {
-		return_code = -4; /* Element counts of 2 files mismatch */
-		goto cleanup;
-	}
+  if (image_cnt != label_cnt) {
+  return_code = -4; /* Element counts of 2 files mismatch */
+  goto cleanup;
+  }
 
-	for (i = 0; i < 2; ++i) {
-		fread(tmp, 1, 4, ifp);
-		image_dim[i] = mnist_bin_to_int(tmp);
-	}
+  for (i = 0; i < 2; ++i) {
+  fread(tmp, 1, 4, ifp);
+  image_dim[i] = mnist_bin_to_int(tmp);
+  }
 
-	if (image_dim[0] != 28 || image_dim[1] != 28) {
-		return_code = -2; /* Not a valid image file */
-		goto cleanup;
-	}
+  if (image_dim[0] != 28 || image_dim[1] != 28) {
+  return_code = -2; /* Not a valid image file */
+  goto cleanup;
+  }
 
-	*count = image_cnt;
-	*data = (mnist_data *)malloc(sizeof(mnist_data) * image_cnt);
+  *count = image_cnt;
+  *data = (mnist_data *)malloc(sizeof(mnist_data) * image_cnt);
 
-	for (i = 0; i < image_cnt; ++i) {
-		int j;
-		unsigned char read_data[28 * 28];
-		mnist_data *d = &(*data)[i];
+  for (i = 0; i < image_cnt; ++i) {
+  int j;
+  unsigned char read_data[28 * 28];
+  mnist_data *d = &(*data)[i];
 
-		fread(read_data, 1, 28*28, ifp);
+  fread(read_data, 1, 28*28, ifp);
 
 #ifdef MNIST_DOUBLE
-		for (j = 0; j < 28*28; ++j) {
-			d->data[j/28][j%28] = read_data[j] / 255.0;
-		}
+  for (j = 0; j < 28*28; ++j) {
+  d->data[j/28][j%28] = read_data[j] / 255.0;
+  }
 #else
-		memcpy(d->data, read_data, 28*28);
+  memcpy(d->data, read_data, 28*28);
 #endif
 
-		fread(tmp, 1, 1, lfp);
-		d->label = tmp[0];
-	}
+  fread(tmp, 1, 1, lfp);
+  d->label = tmp[0];
+  }
 
 cleanup:
-	if (ifp) fclose(ifp);
-	if (lfp) fclose(lfp);
+  if (ifp) fclose(ifp);
+  if (lfp) fclose(lfp);
 
-	return return_code;
+  return return_code;
 }
 
 #endif /* MNIST_HDR_ONLY */
